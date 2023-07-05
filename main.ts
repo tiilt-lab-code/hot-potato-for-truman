9a329321bf961de3fac285ad048f279eb3b18236
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2023-07-05 10:46:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Chessboard)
     timer = randint(5, 15)
     while (timer >= 0) {
+        basic.showIcon(IconNames.Heart)
         timer += -1
         basic.pause(1000)
+        basic.showIcon(IconNames.SmallHeart)
     }
     basic.showIcon(IconNames.Skull)
     music.play(music.tonePlayable(294, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
