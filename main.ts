2abcb387296af186f247c9570b3a6f671345652f
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2023-07-05 11:18:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let timer = 0
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Chessboard)
     timer = randint(5, 15)
-    while (timer >= 0) {
+    while (timer > 0) {
         basic.showIcon(IconNames.Heart)
         timer += -1
         basic.pause(1000)
